eda0ee962826d5176099e8c9ae5963a393e2441e
johngutentag <john.m.gutentag@gmail.com>
2020-06-28 15:25:59 -0500

    Almost finished with the plot ranking function

--- script.py
+++ script.py
@@ -3,15 +3,36 @@ import matplotlib.pyplot as plt
 
 # load rankings data here:
 
-
+wood = pd.read_csv('Golden_Ticket_Award_Winners_Wood.csv')
+steel = pd.read_csv('Golden_Ticket_Award_Winners_Steel.csv')
 
 
 
 # write function to plot rankings over time for 1 roller coaster here:
 
+def plot_ranking(dataset, coaster):
+    
+    filter = lambda x: all(name.lower() in x.lower() for name in coaster)
+    df_coaster = dataset.loc[dataset["Name"].apply(filter)]
+    x_values = range(0, dataset['Year of Rank'].nunique())
+    y_values = df_coaster['Rank'].to_list()
+    rev = y_values.reverse()
+    x_labels = df_coaster['Year of Rank'].to_list()
+    print(x_values)
+    print(y_values)
+    print(x_labels)
+    print(rev)
+    
+    ax = plt.subplot()
+    plt.plot(x_values, y_values)
+    ax.set_yticks(y_values)
+    ax.set_xticks(x_values)
+    ax.set_xticklabels(df_coaster['Year of Rank'].to_list())
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Rank')
+    plt.show()
 
-
-
+plot_ranking(wood, 'Boulder Dash')
 
 
 
